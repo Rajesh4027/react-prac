f645b34fd3470c12f7b68b0701417c1b95494ea2
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-07-16 21:13:08 +0530

    jsx works on in hello.js component

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,12 +2,15 @@ import React from "react";
 
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>hello i am jsx</h1>
-        </div>
-    )
+    // return (
+    //     <div className='dummyclass'>
+    //         <h1>hello i am jsx</h1>
+    //     </div>
+    // )
 
+    return React.createElement('div',
+        {id:"hello",className:'dummyclass'},
+        React.createElement('h1',null,"hello i am also jsx"))
 
 }
 
