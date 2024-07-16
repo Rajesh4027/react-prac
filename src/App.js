0d8a67920b87482d679de3279196ec7da1eccdf0
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-07-16 20:55:31 +0530

    functional and class component executing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/greet';
+import Welcome from './components/welcome'
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello />
+
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>hello i am jsx</h1>
+        </div>
+    )
+
+
+}
+
+export default Hello
